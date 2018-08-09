53d0f9ab283cd12e0f5a5f66d9545ff39303aa1b
Thijs <thijsvangogh@gmail.com>
2018-08-09 11:34:54 +0200

    fixed something

--- do_measurement.py
+++ do_measurement.py
@@ -4,14 +4,16 @@ import numpy as np
 from simple_daq import Device
 
 
-dev = Device('/dev/ttyACM0')
+dev = Device('COM5')
 dev.initialize()
 sleep(1)
 out_port = 0  # Output Port
 in_port = 0  # Input Port
 
-voltages = np.linspace(0,4025,100)  # Voltages to sweep
+voltages = np.linspace(0,4025,128)  # Voltages to sweep
 data = np.zeros((100))  # Where to store data
+
+print(dev.idn())
 
 for i in range(len(voltages)):
     dev.set_analog_value(out_port, voltages[i])
